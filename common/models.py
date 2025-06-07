460cbe5db7c2c8c9b340534a5eb18c5da4ccc595
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-07 20:20:17 +0300

    asdasd

--- common/models.py
+++ common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from photos.models import Photo
+
+
 # Create your models here.
+class Comment(models.Model):
+    text = models.TextField(max_length=250)
+    date_of_publication = models.DateField(auto_now_add=True)
+    to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
