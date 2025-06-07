4fcae7f405f4dfed7aa31b491f10c10295090d17
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-07 18:33:07 +0300

    asdasd

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    personal_pet_photo = models.URLField(max_length=200, null=False, blank=False)
+    date_of_birth = models.DateField()
+    slug = models.SlugField(unique=True, null=False, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+            return super().save(*args, **kwargs)
+
+        super().save(*args, **kwargs)
+
